08ccbf415a35ae4a0da5a0cc9551de267ea844ea
bikz05 <bikz05@gmail.com>
2021-01-02 05:22:05 -0800

    Add block name to CFG Node and function to find predecessors.

--- my_examples/mycfg.py
+++ my_examples/mycfg.py
@@ -3,6 +3,7 @@ import sys
 
 TERMINATORS = ("jmp", "br", "ret")
 
+
 # :!bril2json < test/interp/jmp.bril | python3 my_examples/mycfg.py
 def form_blocks(body):
     block = []
@@ -20,30 +21,60 @@ def form_blocks(body):
     if len(block) > 0:
         yield block
 
+
 class Node:
-    def __init__(self, block, block_id, child_block_ids):
+    def __init__(self, block, name, block_id, child_block_ids):
         self.block = block
+        self.name = name
         self.id = block_id
         self.child_block_ids = child_block_ids
+
     def __str__(self):
-        return "block_id {}, block: {}, child_block_ids {}".format(self.id, self.child_block_ids, self.block)
+        return "name {}, block_id {}, child_block_ids {}, block {}".format(
+            self.name, self.id, self.child_block_ids, self.block)
+
 
 def form_cfg(blocks):
     ids_map = {}
+    block_names = {}
     for idx, block in enumerate(blocks):
         if "label" in block[0]:
+            block_names[idx] = block[0]["label"]
             ids_map[block[0]["label"]] = idx
     cfg = []
     for idx, block in enumerate(blocks):
         if "op" in block[-1] and block[-1]["op"] in TERMINATORS:
             if "labels" in block[-1]:
-                child_block_ids = [ids_map[label] for label in block[-1]["labels"]]
+                child_block_ids = [
+                    ids_map[label] for label in block[-1]["labels"]
+                ]
             else:
                 child_block_ids = None
-            cfg.append(Node(block, idx, child_block_ids))
-        else: # Enter next block
-            cfg.append(Node(block, idx, [idx + 1 if idx + 1 < len(blocks) else None]))
+            cfg.append(
+                Node(
+                    block,
+                    block_names[idx] if idx in block_names else "b" + str(idx),
+                    idx, child_block_ids))
+        else:  # Enter next block
+            cfg.append(
+                Node(
+                    block,
+                    block_names[idx] if idx in block_names else "b" + str(idx),
+                    idx, [idx + 1] if idx + 1 < len(blocks) else None))
     return cfg
+
+
+def get_predecessors(cfg):
+    predecessors_map = {}
+    for node in cfg:
+        if node.child_block_ids:
+            for child_block_id in node.child_block_ids:
+                if child_block_id in predecessors_map:
+                    predecessors_map[child_block_id].add(node.id)
+                else:
+                    predecessors_map[child_block_id] = {node.id}
+    return predecessors_map
+
 
 def mycfg():
     prog = json.load(sys.stdin)
@@ -58,6 +89,11 @@ def mycfg():
         print("cfg")
         for node in cfg:
             print(node)
+        print("predecessors_map")
+        predecessors_map = get_predecessors(cfg)
+        for node_id, predecessors in predecessors_map.items():
+            print("{} : {}".format(node_id, predecessors))
+
 
 if __name__ == "__main__":
     mycfg()
